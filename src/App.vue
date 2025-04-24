c5d02b3506391d865e5e38941aead6dcb157a772
Theoden Melgar <theodenmelgar@gmail.com>
2025-04-23 17:51:40 -0700

    Wrote navbar component and added to App.vue, included primary color in styles.css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <script setup>
+  import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+  <nav>
+    <Navbar />
+  </nav>
+
+  <main>
+
+  </main>
 </template>
 
 <style scoped>
+  nav{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 50px;
+  }
+  main{
+    position: absolute;
+    top: 50px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const active = ref(false)
+</script>
+
+<template>
+    <nav class="navbar">
+        <h2>Lopez Urban Farm</h2>
+
+        <div class="btn-group">
+            <button :class="active ? 'active' : 'inactive' ">
+                View
+            </button>
+            <button :class="active ? 'active' : 'inactive' ">
+                Add
+            </button>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+    h2{
+        font-style: normal;
+        color: white;
+    }
+    .navbar{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        height: 50px;
+        padding: 5px 20px;
+        box-sizing: border-box;
+        background: var(--primary-gradient);
+        border: 1px solid green;
+    }
+    .btn-group{
+        display: flex;
+        justify-content: space-evenly;
+        column-gap: 10px;
+    }
+    .btn-group button{
+        padding: 5px 15px;
+        border-width: 1px;
+        border-radius: 5px;
+        text-align: center;
+        color: black;
+    }
+    .btn-group button.active{
+        background-color: lightgray;
+    }
+    .btn-group button.inactive{
+        background-color: white;
+    }
+</style>
