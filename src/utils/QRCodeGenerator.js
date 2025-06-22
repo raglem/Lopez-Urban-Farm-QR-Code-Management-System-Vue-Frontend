ce42c5a911911f6b217422a55084946e2b56d3be
Theoden Melgar <theodenmelgar@gmail.com>
2025-06-22 16:31:13 -0700

    edited QRCodeGenerator.js util to route to frontend url in .env

--- src/utils/QRCodeGenerator.js
+++ src/utils/QRCodeGenerator.js
@@ -1,7 +1,7 @@
 import QRCode from 'qrcode'
 const generateQRCode = async (_id, options = {}) => {
     try{
-        const url = `${import.meta.env.VITE_API_URL}/plant/${_id}`
+        const url = `${import.meta.env.VITE_FRONTEND_URL}/plant/${_id}`
         const qrCode = await QRCode.toDataURL(url, {
             width: 300,
             margin: 2,
